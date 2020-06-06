6dea9108e3ba834df0d66d9a9af7c87cf2a47353
Alexander Chori <alexandrchori@gmail.com>
2020-06-06 16:00:45 +0300

    Add synchronized stress test

---
+++ src/test/kotlin/ru/chori/deque/concurrent/ConcurrentLinkedDequeStressTest.kt
@@ -0,0 +1,54 @@
+package ru.chori.deque.concurrent
+
+import org.junit.Test
+import java.util.*
+import java.util.concurrent.LinkedBlockingDeque
+import kotlin.random.Random
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ConcurrentLinkedDequeStressTest {
+    companion object {
+        const val STRESS_ITERATIONS: Int = 60_000
+    }
+
+    @Test
+    fun stressTest() {
+        val actual: ConcurrentDeque<Int> = ConcurrentLinkedDeque()
+        val expected: Deque<Int> = LinkedBlockingDeque()
+
+        for (i in 0 until STRESS_ITERATIONS) {
+            when (Random.nextInt(5)) {
+                0 -> {
+                    val value: Int = Random.nextInt()
+
+                    actual.addFirst(value)
+                    expected.addFirst(value)
+                }
+                1 -> {
+                    val value: Int = Random.nextInt()
+
+                    actual.addLast(value)
+                    expected.addLast(value)
+                }
+                2 -> {
+                    val value: Int = Random.nextInt()
+
+                    assertEquals(expected.contains(value), actual.contains(value))
+                }
+                3 -> {
+                    assertEquals(expected.pollFirst(), actual.pollFirst())
+                }
+                4 -> {
+                    assertEquals(expected.pollLast(), actual.pollLast())
+                }
+            }
+
+            assertTrue {
+                expected.stream().allMatch {
+                    actual.contains(it)
+                }
+            }
+        }
+    }
+}
